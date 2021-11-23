5ae9872cb5345b6643e991fd920551ec201f9aad
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-11-23 13:27:00 +0530

    added menu bar

--- first-react-project/src/Components/Header.js
+++ first-react-project/src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navigate from './Navigation'
 
 function Header() {
     return(
-        <header className="border-b font-bold p-3">
-            AppName
+        <header className="border-b p-3 flex justify-between items-centre">
+            <span className="font-bold">
+                AppName
+            </span>
+
+            <Navigate/>
         </header>
     )
 }
