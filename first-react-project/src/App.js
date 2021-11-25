2a0cd9a402202249699dc368ccd83386b62df730
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-11-25 23:57:17 +0530

    added react router

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header/>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/" className="text-blue-500">Home</Link>
+          </li>
+        </ul>
+      </Router>
       <Footer/>
     </div>
   );
