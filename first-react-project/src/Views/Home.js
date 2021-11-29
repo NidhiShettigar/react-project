f52e44f313b6f8e477c373431cf7c63168fe3f6e
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-11-29 22:23:11 +0530

    Products added

--- first-react-project/src/Views/Home.js
+++ first-react-project/src/Views/Home.js
@@ -1,11 +1,66 @@
-import React from "react";
-import HelloWorld from "../Components/HelloWorld";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Loader from '../Components/Loader'
 
 function Home() {
+    const url = 'https://reqres.in/api/users?page=2'
+    const [products, setProducts] = useState({
+        loading: false,
+        data: null,
+        error: false
+    })
+
+    
+    useEffect(() => {
+        setProducts({
+            loading: true,
+            data: null,
+            error: false
+        })
+        axios.get(url)
+            .then(response => {
+                setProducts({
+                    loading: false,
+                    data: response.data,
+                    error: false
+                })
+            })
+            .catch(() => {
+                setProducts({
+                    loading: false,
+                    data: null,
+                    error: true
+                })
+            })
+    }, [url])
+
+    let content = null
+
+    if(products.error){
+        content = <p>
+            There was an error please refresh or try again later.
+        </p>
+    }   
+
+    if(products.loading){
+        content = <Loader/>
+    }
+
+    
+    if(products.data){
+        content = products.data.data.map((items,key) =>
+            <div>
+                {items.email}
+            </div>
+        )
+    }
+//8:10
     return(
         <div>
-            <HelloWorld name="Nidhi"/>
-            <h1 className="font-bold text-2xl">This is home page</h1>
+            <h1 className="font-bold text-2xl mb-3">
+                Best Software Engineers
+            </h1>
+            {content}
         </div>
     )
 }
